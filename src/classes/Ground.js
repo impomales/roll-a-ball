faa7ad50c469281a57b6867eb5df2b71cd30d6ac
Isaias Pomales <impomales@gmail.com>
2018-06-05 18:55:34 -0400

    add material to ground

--- src/classes/Ground.js
+++ src/classes/Ground.js
@@ -1,14 +1,14 @@
-import * as BABYLON from "babylonjs";
-import GameObject from "./GameObject";
+import * as BABYLON from 'babylonjs';
+import GameObject from './GameObject';
 
 export default class Ground extends GameObject {
   constructor(size, game) {
-    super("ground", game);
+    super('ground', game);
     const vertexData = BABYLON.VertexData.CreatePlane({ size });
     vertexData.applyToMesh(this);
     // create walls
     const northWall = new BABYLON.MeshBuilder.CreateBox(
-      "wall",
+      'wall',
       {
         width: size,
         heigth: 1,
@@ -19,7 +19,7 @@ export default class Ground extends GameObject {
     northWall.position.y = size / 2;
 
     const southWall = new BABYLON.MeshBuilder.CreateBox(
-      "wall",
+      'wall',
       {
         width: size,
         height: 1,
@@ -30,7 +30,7 @@ export default class Ground extends GameObject {
     southWall.position.y = -size / 2;
 
     const westWall = new BABYLON.MeshBuilder.CreateBox(
-      "wall",
+      'wall',
       {
         width: size - 1,
         height: 1,
@@ -42,7 +42,7 @@ export default class Ground extends GameObject {
     westWall.position.x = 0.5 - size / 2;
 
     const eastWall = new BABYLON.MeshBuilder.CreateBox(
-      "wall",
+      'wall',
       {
         width: size - 1,
         height: 1,
@@ -58,5 +58,17 @@ export default class Ground extends GameObject {
     southWall.parent = this;
     westWall.parent = this;
     eastWall.parent = this;
+
+    // materials
+    const material = new BABYLON.StandardMaterial('groundMaterial', game.scene);
+    material.diffuseColor = new BABYLON.Color3(0.25, 0.5, 1);
+    material.specularColor = BABYLON.Color3.White();
+    material.ambientColor = new BABYLON.Color3(0.1, 0, 0);
+
+    this.material = material;
+    northWall.material = material;
+    southWall.material = material;
+    eastWall.material = material;
+    westWall.material = material;
   }
 }
