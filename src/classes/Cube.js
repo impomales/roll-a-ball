24bd8eaff95df8bb27ad86daf2722d936ab301e0
Isaias Pomales <impomales@gmail.com>
2018-06-06 21:29:40 -0400

    add color to cube class

--- src/classes/Cube.js
+++ src/classes/Cube.js
@@ -6,5 +6,11 @@ export default class Cube extends GameObject {
     super('cube', game);
     const vertexData = BABYLON.VertexData.CreateBox({ size });
     vertexData.applyToMesh(this);
+
+    const material = new BABYLON.StandardMaterial('cubeMaterial', game.scene);
+    material.emissiveColor = new BABYLON.Color3(0.7, 0, 0.5);
+    material.specularColor = new BABYLON.Color3(0, 1, 0);
+
+    this.material = material;
   }
 }
