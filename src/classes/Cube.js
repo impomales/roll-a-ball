a1c480af28106271803b951f20a5138dba1fde3b
Isaias Pomales <impomales@gmail.com>
2018-06-14 08:35:54 -0400

    feat(anim): cubes rotate about y axis

--- src/classes/Cube.js
+++ src/classes/Cube.js
@@ -13,5 +13,34 @@ export default class Cube extends GameObject {
 
     this.material = material;
     game.shadows.getShadowMap().renderList.push(this);
+
+    // animation
+    const animation = new BABYLON.Animation('animCube', 'rotation.y', 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+    const keys = [
+      {
+        frame: 0,
+        value: 0
+      },
+      {
+        frame: 30,
+        value: Math.PI / 2
+      },
+      {
+        frame: 60,
+        value: Math.PI
+      },
+      {
+        frame: 90,
+        value: 3 * Math.PI / 2
+      },
+      {
+        frame: 120,
+        value: 2 * Math.PI
+      }
+    ];
+    animation.setKeys(keys);
+    this.animations.push(animation);
+    game.scene.beginAnimation(this, 0, 120, true, 1.0);
   }
 }
