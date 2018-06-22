adace8f49ce599306399c80afc3ccfb8b6e1d71a
Isaias Pomales <impomales@gmail.com>
2018-06-21 20:32:53 -0400

    fix(path): calls join path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
 app.listen(PORT, () => {
